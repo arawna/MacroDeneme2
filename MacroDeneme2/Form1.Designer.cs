337dce99d44727adb7e5bc1b088591da5f2d6de4
Ali Hocaoglu <alihocaoglu1220@gmail.com>
2021-11-23 15:50:47 +0300

    update

--- MacroDeneme2/Form1.Designer.cs
+++ MacroDeneme2/Form1.Designer.cs
@@ -29,36 +29,100 @@ namespace MacroDeneme2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(371, 193);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(10, 126);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 51);
+            this.button1.Size = new System.Drawing.Size(168, 51);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Ayarla";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(385, 152);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(76, 47);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.Size = new System.Drawing.Size(102, 29);
             this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(76, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(102, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(76, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(102, 29);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(41, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Y:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(41, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(8, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Uyku:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(200, 189);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Arawna";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -69,6 +133,11 @@ namespace MacroDeneme2
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- MacroDeneme2/Form1.cs
+++ MacroDeneme2/Form1.cs
@@ -20,7 +20,9 @@ namespace MacroDeneme2
             InitializeComponent();
         }
         public static Process cmd;
-        public static int Deger = 5;
+        public static int YDeger = 5;
+        public static int XDeger = 0;
+        public static int UykuDeger = 25;
         public static bool Aktif = true;
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -34,6 +36,10 @@ namespace MacroDeneme2
             cmd.StandardInput.WriteLine("Add-Type -AssemblyName System.Windows.Forms");
             cmd.StandardInput.Flush();
 
+            textBox1.Text = YDeger.ToString();
+            textBox2.Text = XDeger.ToString();
+            textBox3.Text = UykuDeger.ToString();
+
             Thread repeaterThread = new Thread(() => ListenerThread());
             repeaterThread.SetApartmentState(ApartmentState.STA);
             repeaterThread.Start();
@@ -41,7 +47,9 @@ namespace MacroDeneme2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Deger = Int32.Parse(textBox1.Text);
+            XDeger = Int32.Parse(textBox2.Text);
+            YDeger = Int32.Parse(textBox1.Text);
+            UykuDeger = Int32.Parse(textBox3.Text);
         }
 
         public static void ListenerThread()
@@ -64,13 +72,13 @@ namespace MacroDeneme2
         }
         public static void Oynat()
         {
-            cmd.StandardInput.WriteLine("$p1 = [System.Windows.Forms.Cursor]::Position.X");
+            cmd.StandardInput.WriteLine("$p1 = [System.Windows.Forms.Cursor]::Position.X + "+ XDeger);
             cmd.StandardInput.Flush();
-            cmd.StandardInput.WriteLine("$p2 = [System.Windows.Forms.Cursor]::Position.Y + "+Deger);
+            cmd.StandardInput.WriteLine("$p2 = [System.Windows.Forms.Cursor]::Position.Y + "+YDeger);
             cmd.StandardInput.Flush();
             cmd.StandardInput.WriteLine("[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($p1, $p2)");
             cmd.StandardInput.Flush();
-            Thread.Sleep(25);
+            Thread.Sleep(UykuDeger);
         }
     }
 }
